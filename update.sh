7c29ebe14ce9bdc230ff61d27dbbcc0ce629dfdd
shadows <ponyofshadows@outlook.com>
2025-03-20 15:46:17 +0800

    backup: Thu Mar 20 15:46:17 CST 2025

--- update.sh
+++ update.sh
@@ -75,7 +75,7 @@ done
 # 保存到github
 git add -A
 echo "===================================================="
-git commit -a -m "backup: $(date)"
+git commit -av -m "backup: $(date)"
 echo "===================================================="
 git push origin master
 
