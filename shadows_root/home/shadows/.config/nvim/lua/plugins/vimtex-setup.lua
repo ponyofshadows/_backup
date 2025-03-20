003df29c0a0da8195dfeaa7f0af912a3a3576941
shadows <ponyofshadows@outlook.com>
2025-03-20 11:18:08 +0800

    backup: Thu Mar 20 11:18:08 CST 2025

--- shadows_root/home/shadows/.config/nvim/lua/plugins/vimtex-setup.lua
+++ shadows_root/home/shadows/.config/nvim/lua/plugins/vimtex-setup.lua
@@ -25,23 +25,23 @@ return function()
   -- 启用 vimtex 补全
   vim.g.vimtex_complete_enabled = 1
   -- 启动 vimtex 遮盖（conceal）
-  vim.g.vimtex_syntax_conceal = {
-    accents = 1,
-    ligatures = 1,
-    cites = 1,
-    fancy = 1,
-    spacing = 1,
-    greek = 1,
-    math_bounds = 1,
-    math_delimiters = 1,
-    math_fracs = 1,
-    math_super_sub = 1,
-    math_symbols = 1,
-    sections = 0,  -- 不隐藏 section
-    styles = 1
-  }
+  -- vim.g.vimtex_syntax_conceal = {
+    -- accents = 1,
+    -- ligatures = 1,
+    -- cites = 1,
+    -- fancy = 1,
+    -- spacing = 1,
+    -- greek = 1,
+    -- math_bounds = 1,
+    -- math_delimiters = 1,
+    -- math_fracs = 1,
+    -- math_super_sub = 1,
+    -- math_symbols = 1,
+    -- sections = 0,
+    -- styles = 1
+  -- }
   vim.opt.conceallevel = 2
-  vim.opt.concealcursor = ""
+  -- vim.opt.concealcursor = ""
   vim.cmd [[
     highlight Conceal guifg=#ffffff guibg=NONE
     syntax match texMath "\\neq\>" conceal cchar=≠
